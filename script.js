8d3ef245e24b36feb65f1e08d94e1670fad5e6db
Fredrik Byström <fredbstrom@gmail.com>
2019-09-13 15:09:37 +0200

    Fixade lite fel

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 
-var sun = document.getElementById("sun");
-var moon = document.getElementById("moon");
+var bild = document.getElementById("bild");
+var x = true;
 
-// när man klickar på bilden så ändras css så den motssatta bilden visas
-sun.addEventListener('click', function(){
-    sun.style.display = 'none';
-    moon.style.display = 'block';
-});
+// när man klickar på bilden så ändras src i img taggen så den motssatta bilden visas
+bild.addEventListener('click', function(){
+    
 
-moon.addEventListener('click', function(){
-    sun.style.display = 'block';
-    moon.style.display = 'none';
+    if(x === true) {
+        bild.setAttribute("src", "img/moon.jpg");
+        x = false;
+    }else {
+        bild.setAttribute("src", "img/sun.jpg");
+        x = true;
+    }
 });
